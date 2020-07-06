8bddab22c74f50e818d2c0c5739aec50ee5f5a08
ReneStalitza <Renestalitza@web.de>
2020-07-06 21:46:22 +0200

    alert

--- scripts/treemarker.js
+++ scripts/treemarker.js
@@ -1,17 +1,23 @@
 function markTree() {
+    if (active == "" || active == undefined){
+      alert("Please select a tree");
+    }
+    else { 
     
-    item = active;
-    if (item.hasAttribute('affected')) {
-        item.removeAttribute('affected');
-        document.getElementById('isAffected').innerHTML= 'No';
-        checkIfEndangered(item);
-    } else {
-        item.setAttribute('value', 'X');
-        item.setAttribute('color', 'red');
-        item.setAttribute('material', {
-            color: 'red'
-        });
-        item.setAttribute('affected', '');
-        document.getElementById('isAffected').innerHTML= 'Yes';
+        item = active;
+        if (item.hasAttribute('affected')) {
+            item.removeAttribute('affected');
+            document.getElementById('isAffected').innerHTML= 'No';
+            checkIfEndangered(item);
+        }   
+        else {
+            item.setAttribute('value', 'X');
+            item.setAttribute('color', 'red');
+            item.setAttribute('material', {
+                color: 'red'
+            });
+            item.setAttribute('affected', '');
+            document.getElementById('isAffected').innerHTML= 'Yes';
+        }
     }
 }
