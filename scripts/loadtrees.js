5bd861928ba38b97780cbcc176b1932eedb11ccb
Carolin Wortmann <carolin.wortmann@uni-muenster.de>
2020-07-31 11:50:41 +0200

    add doc

--- scripts/loadtrees.js
+++ scripts/loadtrees.js
@@ -1,16 +1,21 @@
+// Options necessary for retrieval of current position.
 var options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
 };
 
+// Stores locations of trees.
 var places;
 
+/*
+ * Load all relevant trees from Repository.
+ * This includes a maximum of 50 trees which are closest to the user's current position up to a maximum distance of * 3000m.
+ */
 function loadTrees() {
     fetch('https://raw.githubusercontent.com/snavas/InteractionWithGeoinformation/master/assets/osmtrees.geojson')
         .then(response => response.json())
         .then(data => places = data);
-
     setTimeout(() => {
         navigator.geolocation.getCurrentPosition(success, error, options);
     }, 3000);
@@ -34,36 +39,43 @@ function success(pos) {
     setTrees();
 }
 
+/*
+ * Calculate relevant trees from all stored trees. 
+ */
 function setTrees() {
     var lat = document.querySelector('a-camera').getAttribute('latVal');
     var lon = document.querySelector('a-camera').getAttribute('lonVal');
 
     var pqueue = new PseudoPQueue(50);
 
+    // Ad tree to PQueue if close enough.
     places.features.forEach((place) => {
         var dist = getDistance(lat, lon, place.geometry.coordinates[1], place.geometry.coordinates[0]);
         pqueue.enqueue(dist, place);
     });
 
+    // Add relevant trees to file. 
     while (!pqueue.isEmpty()) {
         var currPlace = pqueue.deleteMin();
         addPlace(currPlace);
     }
-
-    var texts = document.querySelectorAll('a-text');
-    texts.forEach((txt) => {
-        console.log(txt);
-        console.log(txt.getAttribute('description'));
-    });
 }
 
+/*
+ * Add single tree to scene at given coordinates.
+ */
 function addPlace(place) {
-    var scene = document.querySelector('a-scene');
+    scene = document.querySelector('a-scene');
+
+    // Get attribute values from stored data.
     const latitude = place.geometry.coordinates[1];
     const longitude = place.geometry.coordinates[0];
+    const species = place.species;
 
-    var species = place.species;
+    // Create element. 
     const icon = document.createElement('a-text');
+
+    // Set basic attributes.
     icon.setAttribute('type', '');
     icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
     icon.setAttribute('look-at', '[gps-camera]');
@@ -72,6 +84,8 @@ function addPlace(place) {
     icon.setAttribute('scale', '7 7 7');
     icon.setAttribute('geometry', 'primitive: ring; radiusInner: 0.11; radiusOuter: 0.14');
     icon.setAttribute('align', 'center');
+
+    // Set species and icon accordingly.
     if (species == "" || species == undefined) {
         icon.setAttribute('species', rdmSpecies());
     } else {
@@ -79,9 +93,11 @@ function addPlace(place) {
     }
     checkIfEndangered(icon);
 
+    // Dummy infected trees. 
     var rdmInt = Math.floor((Math.random() * 2) + 1);
     if (rdmInt == 1) {
         markTree(icon);
     }
+
     scene.appendChild(icon);
 }
--- scripts/pqueue.js
+++ scripts/pqueue.js
@@ -1,46 +1,82 @@
+/* 
+ * Class representing a Pair <key, value> of values. 
+ */
 class Pair {
+
+    /*
+     * Constructor
+     */
     constructor(key, value) {
         this.key = key;
         this.value = value;
     }
 
+    /*
+     * Returns the key of this Pair.
+     */
     getKey() {
         return this.key;
     }
 
+    /*
+     * Returns the value of this Pair.
+     */
     getValue() {
         return this.value;
     }
 }
 
+/*
+ * Clsss resembling an implementation of a PriorityQueue. 
+ * The first element ("min") has the maximum key of all the elements in the PQueue 
+ * and is removed if the capacity is reached and an element with a smaller key is 
+ * supposed to be added. 
+ * Intenally a heap structure is used. 
+ */
 class PseudoPQueue {
     constructor(trees) {
         this.data = [];
         this.capacity = 50;
     }
 
+    /* 
+     * Returns whether this PriorityQueue is empty.
+     */
     isEmpty() {
         return this.data.length == 0;
     }
 
+    /* 
+     * Returns the value of the first element. 
+     */
     min() {
         if (this.data.length != 0) {
             return this.data[0].getValue();
         }
     }
 
+    /*
+     * Returns the key of the first element.
+     */
     minKey() {
         if (this.data.length != 0) {
             return this.data[0].getKey();
         }
     }
 
+    /*
+     * Removes the first element and returns its value.
+     */
     deleteMin() {
         var min = this.data[0];
         this.data.splice(0, 1);
         return min.getValue();
     }
 
+    /* 
+     * Enqueues a new element iff there is still capacity or 
+     * if its key is smaller than that of the current first element.
+     */
     enqueue(key, value) {
         if (this.data.length >= this.capacity) {
             this.replace(key, value);
@@ -49,6 +85,9 @@ class PseudoPQueue {
         }
     }
 
+    /* 
+     * Removes current min element and adds the new element at its correct position.
+     */
     replace(key, value) {
         if (this.data[0].key < key) {
             return;
@@ -58,6 +97,9 @@ class PseudoPQueue {
         }
     }
 
+    /* 
+     * Inserts a new element at its correct position.
+     */
     insert(key, value) {
         var current = this.data.length;
         var newElement = new Pair(key, value);
@@ -69,6 +111,9 @@ class PseudoPQueue {
         this.data[current] = newElement;
     }
 
+    /*
+     * Returns the index of the parent element of the given index in the heap. 
+     */
     parent(i) {
         var value = 0;
         if (i != 0) {
